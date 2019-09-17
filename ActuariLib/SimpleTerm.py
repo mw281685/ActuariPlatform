a2f53a257e11f1f59e73b85314b5da5e239b90d7
Malgorzata Smietanka <ucabmw5@ucl.ac.uk>
2019-09-17 22:02:30 +0100

    current version

--- ActuariLib/SimpleTerm.py
+++
@@ -1,7 +0,0 @@
-import Projection
-import numpy as np
-
-class SimpleTerm(Projection.ProjectionPP):
-    
-    def runDecrements(self):
-        self.polsIF_bop = np.ones(self.RunConfig.proj_term)
--- ActuariLib/Test.py
+++
@@ -1,6 +0,0 @@
-class abc(object):
-    """docstring for abc."""
-
-    def __init__(self, arg):
-        super(abc, self).__init__()
-        self.arg = arg
